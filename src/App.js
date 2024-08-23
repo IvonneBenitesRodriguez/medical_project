3a8b546251d03fc5914eab96b6e24998c5f3b062
Ivonne <ivonnebenites2015@gmail.com>
2024-08-23 23:25:41 +0200

    Update file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './components/Header.jsx';
+import FormRegistry from './components/Form.jsx';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <FormRegistry />
+    </div>
+  );
 }
 
 export default App;
